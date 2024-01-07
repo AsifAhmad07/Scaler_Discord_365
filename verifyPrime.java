80ae38bfe8d4737d2b5f5377e72d93a557b801db
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-01-08 00:44:49 +0530

    Day 07

---
+++ verifyPrime.java
@@ -0,0 +1,45 @@
+public class verifyPrime {
+    /*
+     * Day - 07
+     * Name Of THe Problem - Verify Prime
+     * Given a number N, verify if N is prime or not.
+
+Return 1 if N is prime, else return 0.
+
+Example :
+
+Input : 7
+Output : True
+Problem Approach:
+
+VIDEO : https://www.youtube.com/watch?v=7VPA-HjjUmU
+
+Complete code in the hint.
+
+
+     */
+    public class Solution {
+        public int isPrime(int A) {
+             int count = 0;
+            int temp = (int)Math.sqrt(A);
+            for(int i = 1; i  <= temp;i++){
+                if(A % i == 0){
+                    if(i   == A /i){
+                        count++;
+                    }
+                    else{
+                        count +=2;
+                    }
+                }
+    
+            }
+             if(count == 2){
+                return 1;
+             }
+            
+            return 0;
+        }
+    }
+    
+    
+}
